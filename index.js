b35f8539d20cc182983027de346b2adad5cf758c
evanscloud <dijkstrascloud@gmail.com>
2016-12-23 11:18:20 -0500

    Use multiple functions to set scope properly

--- index.js
+++ index.js
@@ -5,12 +5,10 @@ var cake = {
   bakeTemp: "425 degrees",
   bakeTime: "45 minutes",
   customer: "Tommy",
-  decorate: function(updateFunction) {
-    var status = "Decorating with " + this.topping + ". Ready to eat soon!"
-    updateFunction(status)
-    setTimeout(function() {
-      updateFunction(serve.apply(this, "Happy Eating!", this.customer))
-    }, 2000)
+  decorate: function (updateFunction) {
+    var status = "Decorating with " + this.topping + ". Ready to eat soon!";
+    updateFunction(status);
+    setTimeout( () => updateFunction(serve.apply(this, ["Happy Eating!", this.customer])), 2000);
   }
 }
 
@@ -24,13 +22,14 @@ var pie = {
 }
 
 function makeCake() {
-  var updateCakeStatus;
-  mix(updateCakeStatus)
+  var updateCakeStatus = updateStatus.bind(this);
+  mix.call(cake, updateCakeStatus);
 }
 
 function makePie() {
-  var updatePieStatus;
-  mix(updatePieStatus)
+  var updatePieStatus = updateStatus.bind(this);
+  pie.decorate = cake.decorate.bind(pie);
+  mix.call(pie, updatePieStatus);
 }
 
 function updateStatus(statusText) {
@@ -38,39 +37,37 @@ function updateStatus(statusText) {
 }
 
 function bake(updateFunction) {
-  var status = "Baking at " + this.bakeTemp + " for " + this.bakeTime
-  setTimeout(function() {
-    cool(updateFunction)
-  }, 2000)
+  var status = "Baking at " + this.bakeTemp + " for " + this.bakeTime;
+  setTimeout( () => cool.call(this, updateFunction), 2000);
+  updateFunction(status);
 }
 
 function mix(updateFunction) {
-  var status = "Mixing " + this.ingredients.join(", ")
-  setTimeout(function() {
-    bake(updateFunction)
-  }, 2000)
-  updateFunction(status)
+  var status = "Mixing " + this.ingredients.join(", ");
+  setTimeout( () => bake.call(this, updateFunction), 2000);
+  updateFunction(status);
 }
 
 function cool(updateFunction) {
-  var status = "It has to cool! Hands off!"
-  setTimeout(function() {
-    this.decorate(updateFunction)
-  }, 2000)
+  var status = "It has to cool! Hands off!";
+  setTimeout( () => this.decorate(updateFunction), 2000);
+  updateFunction(status);
 }
 
 function makeDessert() {
-  //add code here to decide which make... function to call
-  //based on which link was clicked
+  if (this.parentNode.id === "cake") {
+    makeCake.call(this.parentNode)
+  }
+  else if (this.parentNode.id === "pie") {
+    makePie.call(this.parentNode)
+  }
 }
 
 function serve(message, customer) {
-  //you shouldn't need to alter this function
   return(customer + ", your " + this.name + " is ready to eat! " + message)
 }
 
 document.addEventListener("DOMContentLoaded", function(event) {
-  //you shouldn't need to alter this function
   var cookLinks = document.getElementsByClassName("js-make")
   for(var i=0; i<cookLinks.length; i++) {
     cookLinks[i].addEventListener("click", makeDessert)
